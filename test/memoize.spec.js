9a746eff3a8acf448bf63845659b3023c3b6e4ee
Ruslan Shamsutdinov <Ruslan_Shamsutdinov@epam.com>
2021-12-26 18:14:04 +0300

    add base test

---
+++ test/memoize.spec.js
@@ -0,0 +1,20 @@
+import {describe, beforeEach, it} from 'mocha';
+import {expect} from 'chai';
+import {memoize} from '../src/memoize';
+
+describe( 'base test', () => {
+    it('should be a function', () =>{
+        expect(memoize).to.be.a('function');
+    });
+
+    it('should return undefined if no function provided', ()=>{
+        [undefined, null, 123, {}].forEach((arg) => {
+            expect(memoize(arg)).to.be.an('null');
+        })
+    });
+
+    it('should return some function if function provided', () =>{
+        expect(memoize(()=>{})).to.be.a('function');
+    });
+})
+
